ff82a6a9199f386245a3adbe49ea7714970da84c
sharkz <mosenjemudogoz@gmail.com>
2024-06-04 17:14:10 +0300

    task-item

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../Task';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+  @Input() task:Task={text:"",day:"",reminder:false};
+
+  faTimes=faTimes;
+
+}
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { TASKS } from '../../mock-tasks';
 import { Task } from '../../Task';
 import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from './../task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './tasks.component.html',
+  imports: [CommonModule,TaskItemComponent],
+templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
