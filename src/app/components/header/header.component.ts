dc5cf915810d95638c8dd8b1d1e33099e319d12e
sharkz <mosenjemudogoz@gmail.com>
2024-06-05 15:21:09 +0300

    routerlink fxs

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { ButtonComponent } from '../button/button.component';
 import { Subscription } from 'rxjs';
 import { UiService } from '../../services/ui.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
