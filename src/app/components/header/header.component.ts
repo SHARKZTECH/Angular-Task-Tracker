0f1a26e86533d4f1abc5d43fc074cd93f1bc97a6
sharkz <mosenjemudogoz@gmail.com>
2024-06-05 15:11:40 +0300

    about, route

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { Subscription } from 'rxjs';
 import { UiService } from '../../services/ui.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -15,12 +16,16 @@ export class HeaderComponent {
   showAddTask:boolean=true;
   subscription:Subscription;
 
-  constructor(private uiService:UiService){
+  constructor(private uiService:UiService,private router:Router){
     this.subscription=this.uiService.onToggle().subscribe(value=>this.showAddTask=value);
   }
 
   toggleAddTask(){
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route:string){
+    return this.router.url === route;
   }
 
 
